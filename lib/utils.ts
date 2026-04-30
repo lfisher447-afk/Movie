f7e41fbd199f50fa208bfe0df1d1e07eba89dd9f
lfisher447-afk <lfisher447@gmail.com>
2026-04-30 17:14:11 -0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Tailwind class merger to prevent style conflicts
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const fetcher = (url: string) => fetch(url).then(r => r.json());
